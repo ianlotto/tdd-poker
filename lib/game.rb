478d8327b5b38e1e72fbc2b9ed0827a946fcab94
Ian Lotto <ianlotto@gmail.com>
2013-10-23 12:40:59 -0400

    Completely revamps game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -2,27 +2,32 @@ require 'deck'
 require 'hand'
 
 class Game
-  attr_reader :pool, :players
+  attr_reader :players, :pot, :deck
 
-  def initialize(deck = nil)
-    if deck.nil?
-      deck = Deck.new
-      deck.shuffle
-      @players = []
-    end
-
-    @deck = deck
+  def initialize
+    @deck = Deck.new
+    @players = []
+    @pot = 0
+  end
+  
+  def play
+    play_round until game_over?
+    game_over
   end
 
   def play_round
-    while true
-      deal_cards
-      get_bets
-      handle_discards
-      evaluate_hands
-      dispense_pool
-      return_cards
-    end
+    deck.shuffle
+    unfold_players
+    deal_cards
+    take_bets
+    (end_round && return) if game_over?
+    trade_cards
+    take_bets
+    end_round
+  end
+  
+  def unfold_players
+    players.each(&:unfold)
   end
 
   def add_player
@@ -34,38 +39,81 @@ class Game
   end
 
   def deal_cards
-    @players.each { |player| player.hand = Hand.deal_from(@deck) }
-  end
-
-  def get_bets
-    pool = 0
-    @players.each { |player| pool += player.place_bet }
-    @pool = pool
-  end
-
-  def handle_discards
-    @players.each do |player|
-      discard_indices = player.identify_discards
-      discards = player.hand.return_cards(discard_indices)
-      @deck.return(discards)
-
-      player.hand.add_cards(@deck.take(discards.count))
+    @players.each do |player| 
+      next if player.folded?
+      player.deal_in(deck.deal_hand)
     end
   end
 
-  def identify_hand
+  def take_bets
+    high_bet = 0
+    
+    players.each_with_index do |player, i|
+      next if player.folded?
+      puts "High bet: $#{high_bet}"
+      puts "Player #{i + 1}:"
+      puts player.hand
+      response = player.respond_bet
+      case response
+      when :bet
+        bet = player.get_bet
+        raise "bet must be at least $#{high_bet}" unless bet >= high_bet
+        high_bet = add_to_pot(player.take_bet(bet))
+      when :fold
+        player.fold
+      end
+
+      return if game_over?
+    end
   end
 
-  def evaluate_hands
+  def trade_cards
+    players.each_with_index do |player, i|
+      next if player.folded?
+      puts "Player #{i + 1}:"
+      puts player.hand
+      cards = player.get_cards_to_trade
+      deck.return(cards)
+      player.trade_cards(cards, deck.take(cards.count))
+    end
+  end
+  
+  def end_round
+    show_hands
+    puts "WINNER"
+    puts "#{winner.hand} wins $#{pot} with a #{winner.hand.rank}"
+    winner.receive_winnings(pot)
+    @pot = 0
+  end
+  
+  def winner
+    raise 'game is not over' unless game_over?
+    players.sort.last
+  end
+  
+  def show_hands
+    puts "HANDS"
+    players.each_with_index do |player, i|
+      next if player.folded?
+      puts "#{player.hand} (#{player.hand.rank})"
+    end
   end
 
-  def dispense_pool(winner)
-    winner.recieve_winnings(@pool)
-    @pool = 0
+  def add_to_pot(amount)
+    (@pot += amount) && amount
+  end
+  
+  def game_over?
+    players.select { |player| !player.folded? }.count == 1
   end
 
-  def return_cards
-    @players.each { |player| player.return_cards(@deck) }
+  def add_players(n, buy_in)
+    n.times { @players << Player.buy_in(buy_in) }
   end
+end
 
+def test
+  g = Game.new
+  g.add_players(2, 100)
+  g.play_round
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,112 +3,71 @@ require 'game'
 require 'player'
 
 describe Game do
-  subject(:game) { Game.new(deck) }
+  subject(:game) { Game.new }
+  its(:pot) { should == 0 }
+  
+  describe "#deck" do
+    it "should start with a deck" do
+      game.deck.should be_a(Deck)
+    end
+    
+    it "should start with a full deck" do
+      game.deck.count.should == 52
+    end
+  end
+  
+  describe "#add_players" do
+    it "should create the specified number of players" do
+      game.add_players(2,100)
+      game.players.length.should == 2
+    end
+    
+    it "should create players" do
+      game.add_players(3, 100)
+      game.players.first.should be_a(Player)
+    end
+    
+    it 'should set the players bankrolls' do
+      game.add_players(3, 100)
+      game.players.all? { |player| player.bankroll == 100 }.should be_true
+    end
+  end
+  
+  describe "#game_over?" do
+    it 'should return false when players still have money' do
+      game.add_players(5, 100)
+      game.should_not be_game_over
+    end
 
-  let(:deck)    { Deck.new }
-  let(:players) { [player1, player2] }
-  let(:player1) { Player.new("player1", 100) }
-  let(:player2) { Player.new("player2", 200) }
-
-  describe "#add_player" do
-    it "adds a player to players array" do
-      game.stub!(:gets).and_return("1")
-      expect do
-        game.add_player
-      end.to change { game.players.length }
+    it 'should return true when all but one player has no more money' do
+      game.add_players(5, 0)
+      game.add_players(1, 100)
+      game.should be_game_over
     end
   end
 
   describe "#deal_cards" do
-    it "deals hand to all players" do
+    before(:each) { game.add_players(5, 100) }
+    
+    it 'should give each player a full hand' do
       game.deal_cards
-      game.players.all? { |player| player.hand.count == 5 }.should == true
+      game.players.all? { |player| player.hand }.should be_true
+    end
+
+    it 'should not give a player a hand if the player has no money' do
+      game.add_players(1, 0)
+      game.deal_cards
+      game.players.last.hand.should be_nil
     end
   end
-
-  describe "#get_bets" do
-    it "calls bet on all players" do
-      game.players.all? { |player| player.should_receive(:place_bet) }.should == true
+  
+  describe "#add_to_pot" do
+    it "should add the specified amount to the pot" do
+      expect { game.add_to_pot(100) }.to change { game.pot }.by(100)
     end
-
-    it "holds a pool of bets" do
-      expect do
-        game.get_bets
-      end.to change { game.pool }
-    end
-
-  end
-
-  describe "#handle_discards" do
-    it "places discards onto the bottom of the deck" do
-      expect do
-
-        player.stub!(:gets).and_return("1")
-        game.handle_discards
-      end.to_not change { game.deck.length }
-    end
-
-
-    it "returns the same number of cards to the player" do
-      expect do
-
-        player.stub!(:gets).and_return("1")
-        game.handle_discards
-      end.to_not change { game.player[0].hand.count }
-    end
-
-  end
-
-  describe "#identify_hand" do
-    it "identifies the type of hand" do
-      deck = Deck.new
-      hand = Hand.deal_from(deck)
-
-      game.identify_hand(hand).should == :straight_flush
+    
+    it "should return the amount added" do
+      game.add_to_pot(100).should == 100
     end
   end
-
-  describe "#evaluate_hands" do
-    it "identifies the best hand" do
-      deck = Deck.new
-      hand = Hand.deal_from(deck)
-      hand2 = Hand.deal_from(deck)
-
-      game.evaluate_hands([hand, hand2]).should == 1
-    end
-  end
-
-  describe "#dispense_pool" do
-    it "dispenses the pool to the winner" do
-      expect do
-        game.pool = 1
-        winner = Player.new("Unlce Bob", 1_000)
-        game.dispense_pool(winner)
-      end.to change { winner.bankroll }
-    end
-  end
-
-  describe "#return_cards" do
-    it "returns all cards to the deck" do
-      expect do
-        game.return_cards
-      end.to change { game.deck.count }
-    end
-  end
-
-  describe "#play_round" do
-    it "goes through the steps of a round" do
-      [:deal_cards,
-        :get_bets,
-        :replace_discards,
-        :evaluate_hands,
-        :dispense_pool,
-        :return_cards].each do |step|
-        game.should_receive(step).ordered
-      end
-
-      game.play_round
-    end
-  end
-
 end
